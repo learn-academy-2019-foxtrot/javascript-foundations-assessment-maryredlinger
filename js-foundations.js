0e25abce1e520f4a5dafe0389074931ba905e781
Ubuntu <ubuntu@ip-172-31-26-39.us-east-2.compute.internal>
2019-12-02 01:22:59 +0000

    completed

--- js-foundations.js
+++ js-foundations.js
@@ -3,11 +3,39 @@
 
 // 1. Write a function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
 
+var test1 = 15
+var test2 = 0
+var test3 = 385
+var test4 = -7
+
+
+const isDivisible = (number) => {
+    if(number%3 === 0){
+        return `${number} is divisble by 3!`
+    } else {
+        return `${number} is not divisible by 3!`
+    }
+}
+
+console.log(isDivisible(Math.abs(test1)));
+console.log(isDivisible(Math.abs(test2)));
+console.log(isDivisible(Math.abs(test3)));
+console.log(isDivisible(Math.abs(test4)));
 
 
 // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
 
 
+    var helloMe = { name : "Mary",
+    lastName : "Redlinger",
+    city : "San Diego",
+    age : 23,
+        getData: function() {
+            return this.name + " " + this.lastName + " is " + this.age + " years ols and lives in " + this.city
+        }
+    }
+    
+    console.log(helloMe.getData())
 
 // ------------------------------------- Consider the variable:
 
@@ -15,15 +43,41 @@ var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "tem
 
 // 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
 
+const everyOther = (array) => {
+    let newArr = array.filter((value, index) =>{
+        return index%2 === 0
+    })
+     return newArr.join(" ");
+}
 
+console.log(everyOther(randomNouns))
 
 // 3b. Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+
+var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
+
+const capitalizeThat = (array) => {
+    return array.map(value => {
+        return value[0].toUpperCase() + value.slice(1)
+    })
+}
+console.log(capitalizeThat(randomNouns))
 
 
 
 // 3c. STRETCH: Create a function that returns the length of each string from the randomNouns variable. Expected output: [10, 6, 5, 10, 6, 11, 8]
 
+//create function returnLength
+// pass in array
+//find highest index of value
+//return that index in new array
 
+const returnLength = (array) => {
+    return array.map(value => {
+        return value.length
+    })
+}
+console.log(returnLength(randomNouns))
 
 
 // -------------------------------------- Consider the variables:
@@ -33,10 +87,38 @@ var testString3 = "sandiego"
 
 // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
 
+//create a function called alphabetSoup
+//passes in one argument
+//returns a string with letters in alphabetical order
 
+const alphabetSoup = (string) => {
+    let arr = string.split('');
+    let newArr = arr.sort();
+    return newArr.join('');
+}
+console.log(alphabetSoup(testString1))
+console.log(alphabetSoup(testString2))
+console.log(alphabetSoup(testString3))
 
 // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
 
+var testString1 = "learn"
+var testString2 = "academy"
+var testString3 = "sandiego"
+
+//write a function called threeVar
+//combine arrays
+//sort and join arrays
+//returns string with all in alphabetical order
+
+var combine = testString1 + testString2 + testString3
+
+const threeVariableSoup = (string) => {
+    let arr = string.split('');
+    let newArr = arr.sort();
+    return newArr.join('');
+}
+console.log(threeVariableSoup(combine))
 
 
 // ------------------------------------- Consider the variables:
@@ -45,6 +127,36 @@ var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguin
 
 // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
 
+//write function called orderedString
+//pass two variables
+//oder variables based on index number
+//return new string of values
+
+
+const orderedString = (array1, array2) => {
+    return array1.map((v,i) => {
+        return [v + " " + array2[i]]
+    }).join(' ')
+}
+
+console.log(orderedString(amounts, animals));
 
 
 // 5b. STRETCH: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
+
+//create a function called noStringS
+//take in both variables
+//if amounts === 1
+//return animal slice()
+
+const noStringS = (array1, array2) => {
+    return array1.map((v,i) => {
+        if( v === 1){
+        return [v + " " + array2[i].slice(0, array2[i].length -1)]
+        } else {
+        return [v + " " + array2[i]]
+        }
+    }) .join(' ')
+}
+
+console.log(noStringS(amounts, animals));
--- js-foundations.md
+++ js-foundations.md
@@ -15,56 +15,63 @@ logIt()
 
 1a. Look at this Javascript function and try to predict what the console show.
 
+  the console will show nothing because the call for the function isnt passing in a variable 
 
 
 1b. Test the function. Explain why the function returned what it did.
 
-  Your answer:
+  Your answer: The function returned "undefined" because there wasnt a variable being passed through the call for the function in logIt()
 
-  Researched answer:
+  Researched answer: 
 
 
 2. What is JSON?
 
-  Your answer:
+  Your answer: JSON is a form of object that has multiple keys and values in its variable
 
-  Researched answer:
+  Researched answer: JSON stands for JavaScript Object Notation. JSON is a lightweight format for storing and transporting data.
+
 
 
 3. What does CRUD stand for?
 
-  Your answer:
+  Your answer: Create Read Update Delete
 
-  Researched answer:
+  Researched answer: Create Read Update Delete
 
 
 
 4. What does are the 5 HTTP verbs?
 
-  Your answer:
+  Your answer: I have no idea
 
-  Researched answer:
+  Researched answer: POST, GET, PUT, PATCH, and DELETE
 
 
 5. What is a higher-order function?
 
-  Your answer:
+  Your answer: It has to do with loops and index and arrays
 
-  Researched answer:
+  Researched answer: A higher order function does atleast one of the following : takes one or more functions as arguments , and returns a function as its result.
+
+
 
 
 6. STRETCH: What is a closure, what is it good for and how do you recognize one?
 
-  Your answer:
+  Your answer: I dont know
 
-  Researched answer:
+  Researched answer: The most simple way to think of a closure is a function that can be stored as a variable (referred to as a "first-class function"), that has a special ability to access other variables local to the scope it was created in.
+
+
 
 
 7. STRETCH: What is an API?
 
-  Your answer:
+  Your answer: Application Programming Interface
 
-  Researched answer:
+  Researched answer: a set of functions and procedures allowing the creation of applications that access the features or data of an operating system, application, or other service.
+
 
 
 ### Additional Feedback Questions.
@@ -75,6 +82,8 @@ logIt()
 
 2. What was your favorite topic this week?
 
-
+I am enjoying getting to understand React more
 
 3. What was your "A-ha!" moment this week?
+
+I had a lot of "a-ha!" moments during pig latin. I knew what i wanted to do but couldnt figure out how to start. However, mid way through the day it begin to click and I was able to understand the code I was writing and why
